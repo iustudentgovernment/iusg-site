3be737d689919bccc510acd2e20be7da7ec9bfca
dselarka <dselarka@iu.edu>
2020-10-23 18:19:14 -0400

    Created controller file to query database for info to fill in templates

---
+++ backend/src/main/kotlin/edu/indiana/iustudentgovernment/controllers/FillTemplates.kt
@@ -0,0 +1,19 @@
+package edu.indiana.iustudentgovernment.controllers
+
+import edu.indiana.iustudentgovernment.authentication.Member
+import edu.indiana.iustudentgovernment.authentication.getUser
+import edu.indiana.iustudentgovernment.data.getMap
+import edu.indiana.iustudentgovernment.database
+import edu.indiana.iustudentgovernment.http.HandlebarsContent
+import edu.indiana.iustudentgovernment.http.respondHbs
+import edu.indiana.iustudentgovernment.models.IusgBranch
+import edu.indiana.iustudentgovernment.models.IusgBranch.EXECUTIVE
+import edu.indiana.iustudentgovernment.models.IusgBranch.JUDICIAL
+import edu.indiana.iustudentgovernment.models.IusgBranch.LEGISLATIVE
+import edu.indiana.iustudentgovernment.models.Statement
+import edu.indiana.iustudentgovernment.utils.nullifyEmpty
+import io.ktor.application.call
+import io.ktor.response.respondRedirect
+import io.ktor.routing.Route
+import io.ktor.routing.get
+import io.ktor.routing.route
